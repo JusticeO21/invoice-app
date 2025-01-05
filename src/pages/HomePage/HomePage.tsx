e3d3d0e7b4f9ad4270dd2321ae52a0a5301cb017
Justice <justice.owusu@amalitech.com>
2025-01-05 10:50:55 +0000

    Add the NewInvoiceForm to the homePage so it can be rendered on addNewInvoice Button Click

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,32 +1,34 @@
 import InvoiceGroup from "../../components/InvoiceGroup/InvoiceGroup";
 import InvoiceControlBar from "../../components/InvoiceControlBar/InvoiceControlBar";
-import { useAppSelector } from "../../Hooks/useRedux";
-import { useState } from "react";
-import Dialog from "../../components/Dialog/Dialog";
+import { useAppSelector, useAppDispatch } from "../../Hooks/useRedux";
+import NewInvoiceForm from "../NewInvoiceForm/NewInvoiceForm";
+import { addNewInvoice as showNewInvoiceForm } from "../../Redux/invoiceReducer";
 
 function HomePage() {
-    const { invoiceList: data, FilterBy: filterByStatus } = useAppSelector(state => state.invoice);
-    const [addNewInvoiceModalOpen, setAddNewInvoiceModalOpen] = useState(false);
-      
-       function handleNewInvoice() {
-         setAddNewInvoiceModalOpen(true);
-       }
-    
-       function handleNewInvoiceClose() {
-         setAddNewInvoiceModalOpen(false);
-      }
-    return (
-        <>
-            <InvoiceControlBar onAddNewInvoice={handleNewInvoice}/>
-            <InvoiceGroup data={data} filterByStatus={filterByStatus} isError={false} isLoading={false} />
-            {
-                addNewInvoiceModalOpen && (<Dialog isOpen={addNewInvoiceModalOpen} onClose={handleNewInvoiceClose}>
-                        .
-                </Dialog>)
-            }
-        </>
-    )
+  const {
+    invoiceList: data,
+    FilterBy: filterByStatus,
+    addNewInvoice,
+  } = useAppSelector((state) => state.invoice);
+  const dispatch = useAppDispatch();
+
+  function handleAddNewInvoice() {
+    dispatch(showNewInvoiceForm(true));
+    console.log(addNewInvoice);
+  }
+
+  return (
+    <>
+      <InvoiceControlBar onAddNewInvoice={handleAddNewInvoice} />
+      <InvoiceGroup
+        data={data}
+        filterByStatus={filterByStatus}
+        isError={false}
+        isLoading={false}
+      />
+      <NewInvoiceForm />
+    </>
+  );
 }
 
-
-export default HomePage
+export default HomePage;
